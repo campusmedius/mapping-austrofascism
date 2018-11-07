5aad078f5517cdafa848c39a6bdb9cc867537fc0
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2018-11-07 01:34:25 +0100

    config production packages

--- nix/overlays/pkgs/cm-backend/default.nix
+++ nix/overlays/pkgs/cm-backend/default.nix
@@ -20,8 +20,8 @@ stdenv.mkDerivation {
   
   src = fetchgit {
       url = https://github.com/campusmedius/campusmedius.git;
-      rev = "00065c076c2efac735959262d7d01bc85208cfe2";
-      sha256 = "0yjdki03i5rpc09k0gs726j9fw0a5zj8c1bsgsxwy9ihxpdypvzy";
+      rev = "cb7f253d4399ffc2cc6212e5d5d86389142377a0";
+      sha256 = "1x97lkrn8b58pc6h3psyfl0la7g33alsbs8333wr6yhb3xprp1n6";
   };
 
   buildInputs = [ uwsgi-python pythonenv ];
