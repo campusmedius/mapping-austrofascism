3d2928e40cb06a8d345911792d279a2c8b99087f
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2018-12-28 17:31:45 +0100

    configure production packages

--- nix/overlays/pkgs/cm-backend/default.nix
+++ nix/overlays/pkgs/cm-backend/default.nix
@@ -12,7 +12,7 @@ let
 in
 
 stdenv.mkDerivation {
-  name = "cm-backend-0.0.7";
+  name = "cm-backend-0.0.8";
   
   pythonenv = pythonenv;
   python = python;
@@ -20,8 +20,8 @@ stdenv.mkDerivation {
   
   src = fetchgit {
       url = https://github.com/campusmedius/campusmedius.git;
-      rev = "ff4a44b6d828fbf8ba3417889be08b5048a075b9";
-      sha256 = "1hha5i18x6hg09gi3b027vr52973in2861hz21k78aaxmb27p1mi";
+      rev = "2a15fb06270845c1a58547839134ee0b9b052284";
+      sha256 = "0349xb0hby1ncx42kk32fppgw99iwj4l8215w74dp07b9bj3lz01";
   };
 
   buildInputs = [ uwsgi-python pythonenv ];
--- nix/overlays/pkgs/cm-frontend/default.nix
+++ nix/overlays/pkgs/cm-frontend/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchgit}:
 
 stdenv.mkDerivation {
-  name = "cm-frontend-0.0.6";
+  name = "cm-frontend-0.0.7";
   
   src = fetchgit {
       url = https://github.com/campusmedius/campusmedius.git;
-      rev = "ff4a44b6d828fbf8ba3417889be08b5048a075b9";
-      sha256 = "1hha5i18x6hg09gi3b027vr52973in2861hz21k78aaxmb27p1mi";
+      rev = "2a15fb06270845c1a58547839134ee0b9b052284";
+      sha256 = "0349xb0hby1ncx42kk32fppgw99iwj4l8215w74dp07b9bj3lz01";
   };
   
   installPhase = ''
