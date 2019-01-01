2354233c4aae9294d037a16aeb1b69518ce23817
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2019-01-01 22:00:11 +0100

    configure production packages

--- nix/overlays/pkgs/cm-frontend/default.nix
+++ nix/overlays/pkgs/cm-frontend/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchgit}:
 
 stdenv.mkDerivation {
-  name = "cm-frontend-0.0.8";
+  name = "cm-frontend-0.0.9";
   
   src = fetchgit {
       url = https://github.com/campusmedius/campusmedius.git;
-      rev = "8e3326dc9903dec79bdc53994953c6fcceb2c282";
-      sha256 = "0nnnkbgfsr3i8kid24qkmdibaqbc4z2v45a18qqmhp96ww54iwrv";
+      rev = "17ea1f3b47b6be1bf3cba31eccf4b6f1a467ac02";
+      sha256 = "0cgvhvbvm0c145a9ixrqzqm1qscqfzrx669vgyz98w5dwj9prl98";
   };
   
   installPhase = ''
