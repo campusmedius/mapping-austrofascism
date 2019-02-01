ee68ac78e5ead656c5f567c7e59b71158e265ba8
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2019-02-02 00:39:55 +0100

    configure production packages

--- nix/overlays/pkgs/cm-frontend/default.nix
+++ nix/overlays/pkgs/cm-frontend/default.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
   
   src = fetchgit {
       url = https://github.com/campusmedius/campusmedius.git;
-      rev = "ea10fa8d5a74ae41a9b92f938445153d806d1ee8";
-      sha256 = "1l3szaplr85vwnpi122q68m6p47xqhi71ypnnxjggpw8v1hc6i7n";
+      rev = "4c0234668b6fc44d72688f61f0ae8ce6faa2b99a";
+      sha256 = "1h77ra5y0l4qxcbpa6skq6h5r7kzqkj31r7029497fkxgqjyp3c7";
   };
   
   installPhase = ''
