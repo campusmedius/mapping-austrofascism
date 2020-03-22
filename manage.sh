ec1b0f62867742d97958bc86f8d3de0118a91225
Andreas Krimbacher <a.krimbacher@zamg.ac.at>
2020-03-22 19:14:56 +0100

    shorten ansi color variables

--- manage.sh
+++ manage.sh
@@ -10,12 +10,12 @@ if [ "$_arg_command" = "env" ]
 then
   if [ "$_arg_sub_command" = "backend" ]
   then
-    echo -e "$(text-style yellow)Change into backend environment$(text-style reset)"
+    echo -e "$(_ts y)Change into backend environment$(_tr)"
     cd backend/campusmedius
 
     if [ "$_arg_editor" = on ]
     then
-      echo -e "$(text-style yellow)Run editor$(text-style reset)"
+      echo -e "$(_ts y)Run editor$(_tr)"
       nix-shell --command "emacs"
     else
       nix-shell
@@ -23,12 +23,12 @@ then
   fi
   if [ "$_arg_sub_command" = "frontend" ]
   then
-    echo -e "$(text-style yellow)Change into frontend environment$(text-style reset)"
+    echo -e "$(_ts y)Change into frontend environment$(_tr)"
     cd frontend/campusmedius
     
     if [ "$_arg_editor" = on ]
     then
-      echo -e "$(text-style yellow)Run editor$(text-style reset)"
+      echo -e "$(_ts y)Run editor$(_tr)"
       nix-shell --command "emacs"
     else
       nix-shell
@@ -37,15 +37,18 @@ then
   # setup a development environment, frontend, backend server and editor and start chrome
   if [  "$_arg_sub_command" = "develop" ]
   then
-    echo -e "$(text-style yellow)Setup development environment$(text-style reset)"
-    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh run backend;'"
-    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh run frontend;'"
-    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh env backend --editor;'"
-    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh env frontend --editor;'"
-    sleep 3
-    echo -e "$(text-style yellow)Opening backend in browser$(text-style reset)"
+    echo -e "$(_ts y)Setup development environment$(_tr)"
+    echo -e "$(_ts y)Start backend$(_tr)"
+    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh run backend;'" &
+    echo -e "$(_ts y)Start frontend$(_tr)"
+    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh run frontend;'" &
+    echo -e "$(_ts y)Open backend editor$(_tr)"
+    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh env backend --editor;'" &
+    echo -e "$(_ts y)Open frontend editor$(_tr)"
+    $_arg_terminal -e "bash -c 'cd $working_dir; ./manage.sh env frontend --editor;'" &
+    echo -e "$(_ts y)Opening backend in browser$(_tr)"
     xdg-open http://localhost:8000/admin &
-    echo -e "$(text-style yellow)Opening frontend in browser$(text-style reset)"
+    echo -e "$(_ts y)Opening frontend in browser$(_tr)"
     xdg-open http://localhost:4200 &
   fi
 fi
@@ -55,11 +58,11 @@ if [ "$_arg_command" = "init" ]
 then
   if [ "$_arg_sub_command" = "backend" ]
   then
-    echo -e "$(text-style red)Nothing to do$(text-style reset)"
+    echo -e "$(_ts r)Nothing to do$(_tr)"
   fi
   if [ "$_arg_sub_command" = "frontend" ]
   then
-    echo -e "$(text-style yellow)Init frontend angular project$(text-style reset)"
+    echo -e "$(_ts y)Init frontend angular project$(_tr)"
     cd frontend/campusmedius
     nix-shell --command "npm install"
   fi
@@ -70,13 +73,13 @@ if [ "$_arg_command" = "run" ]
 then
   if [ "$_arg_sub_command" = "backend" ]
   then
-    echo -e "$(text-style yellow)Run backend server$(text-style reset)"
+    echo -e "$(_ts y)Run backend server$(_tr)"
     cd backend/campusmedius
     nix-shell --command "python campusmedius/manage.py runserver"
   fi
   if [ "$_arg_sub_command" = "frontend" ]
   then
-    echo -e "$(text-style yellow)Run frontend dev server$(text-style reset)"
+    echo -e "$(_ts y)Run frontend dev server$(_tr)"
     cd frontend/campusmedius
     nix-shell --command "ng serve"
   fi
