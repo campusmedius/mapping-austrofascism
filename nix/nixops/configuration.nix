337f55aaaa3e49a02f8faa3f24fbaaced0e274d8
Andreas Krimbacher <a.krimbacher@zamg.ac.at>
2020-03-30 00:13:50 +0200

    add key

--- nix/nixops/configuration.nix
+++ nix/nixops/configuration.nix
@@ -112,8 +112,8 @@
                 alias = "${pkgs.cm-frontend}/share/campusmedius/viewer/";
                 extraConfig = ''
                     try_files $uri $uri/ /index.html;
-                    auth_basic campusmedius;
-                    auth_basic_user_file /run/keys/basicAuth;
+                    #auth_basic campusmedius;
+                    #auth_basic_user_file /run/keys/basicAuth;
                     expires 10m;
                     etag off;
                 '';
@@ -128,7 +128,7 @@
     
     users.extraUsers.nginx.extraGroups = ["keys"];
 
-    users.extraUsers.root.openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCZM1p/W9r2LY0Klbl1eFhBgGK8Pd+X7XbLJdNwUbEp0KHcqQ0aLuC9+x2hjJJkRTXH8lsN5baWicqEAL+okHmYjkniOgCoreEVEDa8oLxE4o4kTetcfnQ8iCiSNyYIzexPfDKC8fDxt0MaFf2egctd1jaSr4tkUoxGb5VgqtjYNmRlBlZgdnh34FCMUlrgF/hiSNXbQCgKo8GlU83gwc8W2YsdRIos0rarz+aFyvIexphwFos2JXpBhJE6dJkqWsJXHem3i9a2uuSMM8xts+04i09Vm1wby3ww3kvR+rVcHGI/EtO/FSRU3dzIXRZmkEHAHAa6jjZsG4L2+uWZ3dD5 nd@nixos"];
+    users.extraUsers.root.openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCZM1p/W9r2LY0Klbl1eFhBgGK8Pd+X7XbLJdNwUbEp0KHcqQ0aLuC9+x2hjJJkRTXH8lsN5baWicqEAL+okHmYjkniOgCoreEVEDa8oLxE4o4kTetcfnQ8iCiSNyYIzexPfDKC8fDxt0MaFf2egctd1jaSr4tkUoxGb5VgqtjYNmRlBlZgdnh34FCMUlrgF/hiSNXbQCgKo8GlU83gwc8W2YsdRIos0rarz+aFyvIexphwFos2JXpBhJE6dJkqWsJXHem3i9a2uuSMM8xts+04i09Vm1wby3ww3kvR+rVcHGI/EtO/FSRU3dzIXRZmkEHAHAa6jjZsG4L2+uWZ3dD5 nd@nixos" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/gK4eoqB+DtCL9kj3NJfRZV4xGW8tratfW8DIf2UhR akrim@zaanb20"];
 
     # This value determines the NixOS release with which your system is to be
     # compatible, in order to avoid breaking some software such as database
